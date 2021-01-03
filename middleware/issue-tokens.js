a697a843cd141e3f3ac03d1ca8a6ab0760f7b768
Yeasir H <yeasir01@gmail.com>
2021-01-02 19:22:13 -0800

    set cookie options

--- middleware/issue-tokens.js
+++ middleware/issue-tokens.js
@@ -6,17 +6,17 @@ const fs = require('fs');
 //black list all old token when generating new ones.
 module.exports = (req, res) => {
     try {
-        let apkPath = path.resolve(__dirname, "../config/access_private.pem");
-        let rpkPath = path.resolve(__dirname, "../config/refresh_private.pem");
-        let apk = fs.readFileSync(apkPath, 'utf8');
-        let rpk = fs.readFileSync(rpkPath, 'utf8');
+        let apk = fs.readFileSync(path.resolve(__dirname, "../config/access_private.pem"), 'utf8');
+        let rpk = fs.readFileSync(path.resolve(__dirname, "../config/refresh_private.pem"), 'utf8');
         let issuedAt = Math.floor(Date.now() / 1000);
+        let refreshTTL = 30 * 24 * 60 * 60;
+        let accessTTL = 15 * 60;
         let user = req.user;
 
         if (!user) {
             throw new Error("User does not exist on the request object. Unable to generate tokens!")
         };
-        
+
         let payload = {
             iat: issuedAt,
             sub: user._id
@@ -24,14 +24,14 @@ module.exports = (req, res) => {
 
         let accessOpt = {
             algorithm: "RS256",
-            expiresIn: "15m",
+            expiresIn: accessTTL,
             issuer: process.env.ISSUER,
             audience: process.env.AUDIENCE
         };
 
         let refreshOpt = {
             algorithm: 'RS256',
-            expiresIn: "30d",
+            expiresIn: refreshTTL,
             issuer: process.env.ISSUER,
             audience: process.env.AUDIENCE
         };
@@ -39,13 +39,20 @@ module.exports = (req, res) => {
         let accessToken = JWT.sign(payload, apk, accessOpt);
         let refreshToken = JWT.sign(payload, rpk, refreshOpt);
 
-        return res.status(200)
-            .cookie('access_token', accessToken)
-            .cookie('refresh_token', refreshToken)
-            .json({
-                success: true,
-                status: 200
-            });
+        res.cookie('access_token', accessToken, {
+            httpOnly: true,
+            maxAge: accessTTL * 1000
+        })
+
+        res.cookie('refresh_token', refreshToken, {
+            httpOnly: true,
+            maxAge: refreshTTL * 1000
+        })
+
+        return res.status(200).json({
+            success: true,
+            status: 200
+        });
 
     } catch (e) {
         console.log(e);
